c6b077cc18c97062e4e6f083bc8c6e859a80084d
henryfrstr <henryfrstr@gmail.com>
2021-11-20 21:59:42 +0300

    feat: add flight app

--- main/settings.py
+++ main/settings.py
@@ -39,17 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    #drf
+
+    # drf
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
-    
+
     # swagger
     'drf_yasg',
 
-    #my apps
+    # my apps
     'accounts',
+    'flight',
 
 ]
 
@@ -144,10 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- main/urls.py
+++ main/urls.py
@@ -37,7 +37,6 @@ schema_view = get_schema_view(
 )
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -48,8 +47,8 @@ urlpatterns = [
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
-    
+
     path("accounts/", include("accounts.urls")),
+    # path("flight/", include("flight.urls")),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
